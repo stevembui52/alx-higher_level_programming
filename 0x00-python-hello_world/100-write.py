e26ad5d19827930a81a68e18b4c85243a2f9f712
stevembui52 <stevembui52@gmail.com>
2023-09-04 04:33:24 -0700

    please

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,3 +1,9 @@
 import sys
-sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
-sys.exit(1)
+
+def main():
+    message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+    sys.stderr.write(message)
+    sys.exit(1)
+
+if __name__ == "__main__":
+    main()
