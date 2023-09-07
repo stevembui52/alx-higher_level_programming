9e01c632e1bd87ef6244e43bf6e4ec6d7fa8ebaf
stevembui52 <stevembui52@gmail.com>
2023-09-07 06:10:46 -0700

    why

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     import sys
     numarg = len(sys.argv) - 1
     if numarg == 0:
-	print("{:d} arguments.".format(numarg))
+        print("{:d} arguments.".format(numarg))
     elif numarg == 1:
-	print("{:d} argument:".format(numarg))
+        print("{:d} argument:".format(numarg))
     else:
         print("{:d} arguments.".format(numarg))
     for i in range(len(sys.argv)):
