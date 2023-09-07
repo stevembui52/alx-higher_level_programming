b9473614c97e730f45d0ef57c44771d829cfad5c
stevembui52 <stevembui52@gmail.com>
2023-09-07 05:58:34 -0700

    plie

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,13 @@ import sys
 
 numarg = len(sys.argv) - 1
 if numarg == 0:
-    print(f"{numarg:d} arguments.")
+    print("{:d} arguments.".format(numarg))
 elif numarg == 1:
-    print(f"{numarg:d} argument:")
+    print("{:d} arguments.".format(numarg))
 else:
-    print(f"{numarg:d} arguments:")
+    print("{:d} arguments.".format(numarg))
 
 for i in range(len(sys.argv)):
     if i == 0:
         continue
-    print(f"{i:d}: {sys.argv[i]:s}")
+    print("{:d}: {:s}".format(i, sys.argv[i]))
