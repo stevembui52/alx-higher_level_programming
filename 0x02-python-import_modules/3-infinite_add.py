cb7a12f868144195f8f4701d4cdf486c75280fb0
stevembui52 <stevembui52@gmail.com>
2023-09-07 06:17:22 -0700

    correct

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
-from sys import argv
-add = 0
-
-for a in range(1, len(argv)):
-    add += int(argv[a])
-print(add)
+if __name__ == "__main__":
+    from sys import argv
+    add = 0
+    for a in range(1, len(argv)):
+        add += int(argv[a])
+    print(add)
