e4e82bd031e9bcc29af5bfc384752ac54b86f8dc
stevembui52 <stevembui52@gmail.com>
2023-09-07 06:03:21 -0700

    loos

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,9 @@ numarg = len(sys.argv) - 1
 if numarg == 0:
     print("{:d} arguments.".format(numarg))
 elif numarg == 1:
-    print("{:d} arguments.".format(numarg))
+	print("{:d} argument:".format(numarg))
 else:
     print("{:d} arguments.".format(numarg))
-
 for i in range(len(sys.argv)):
     if i == 0:
         continue
