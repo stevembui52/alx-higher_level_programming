31f093937136c1f8de028b7cb1829a2608e14c38
stevembui52 <stevembui52@gmail.com>
2023-09-26 05:20:44 -0700

    Instantiation with optional size: def __init__(self, size=0)

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square
+"""
+
+class Square:
+     """ class Square that defines a square """
+    def __init__(self, size = 0):
+        """ initialize variables """
+	if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
