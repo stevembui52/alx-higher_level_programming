6ec4a269476207e1b1a505a7d72e313b32045244
stevembui52 <stevembui52@gmail.com>
2023-09-26 05:59:10 -0700

    returns the current square area

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -2,26 +2,28 @@
 """
 Square class : defines a square by : (based on 3-square.py)
 """
+
+
 class Square:
     """ class square that defines a square """
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """ initialize attributes """
-	self.size = size
+        self.size = size
 
     @property
     def size(self):
         """ gets the size """
-	return self.__size
+        return self.__size
 
     @size_setter
     def size(self, value):
         """ sets the size property """
-	if type(value) is not int:
+        if type(value) is not int:
             raise TypeError('size must be an integer')
         if (value < 0):
             raise ValueError('size must be >= 0')
         self.__size = value
 
-    def area(self)
+    def area(self):
         """ calculates the area """
-        return self.__size * self.__size)
+        return (self.__size * self.__size)
