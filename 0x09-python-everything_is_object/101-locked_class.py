d82fe499a8d7c7c54321758fa7bbafdb1b348fb6
stevembui52 <stevembui52@gmail.com>
2023-10-03 06:34:33 -0700

    locked

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,8 +3,8 @@
 Class lockedclass defines a locked class
 """
 class LockedClass:
-	def __setattr__(self, name, value):
-		if name == 'first_name':
+    def __setattr__(self, name, value):
+        if  name == 'first_name':
             self.__dict__[name] = value
         else:
             raise AttributeError(f"'LockedClass' object has no attribute 'last_name'")
