2b5d4168329a53d280a84df3539acdefa261d20a
stevembui52 <stevembui52@gmail.com>
2023-10-03 06:36:15 -0700

    locked

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,6 +3,7 @@
 Class lockedclass defines a locked class
 """
 class LockedClass:
+    """ Allow setting the attribute if it's 'first_name' """
     def __setattr__(self, name, value):
         if  name == 'first_name':
             self.__dict__[name] = value
