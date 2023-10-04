e427cd71822a597a21041ea020ad129421a83bdd
stevembui52 <stevembui52@gmail.com>
2023-10-03 23:30:55 -0700

    correction

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """set passsed private attribue of width"""
-        if not(isinstance(value, int)):
+        if not (isinstance(value, int)):
             raise TypeError("width must be an integer")
         elif (value < 0):
             raise TypeError("width must be >= 0")
@@ -37,7 +37,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """set passsed private attribue of width"""
-        if not(isinstance(value, int)):
+        if not (isinstance(value, int)):
             raise TypeError("height must be an integer")
         elif (value < 0):
             raise TypeError("height must be >= 0")
--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -24,7 +24,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """set passsed private attribue of width"""
-        if not(isinstance(value, int)):
+        if not (isinstance(value, int)):
             raise TypeError("width must be an integer")
         elif (value < 0):
             raise TypeError("width must be >= 0")
@@ -38,7 +38,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """set passsed private attribue of width"""
-        if not(isinstance(value, int)):
+        if not (isinstance(value, int)):
             raise TypeError("height must be an integer")
         elif (value < 0):
             raise TypeError("height must be >= 0")
