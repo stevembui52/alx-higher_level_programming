1fb5fab6acd89a3234529e0af25476840853438c
stevembui52 <stevembui52@gmail.com>
2023-12-11 09:07:18 -0800

    light

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-/* Script that prints the addition of 2 integers */
-
-const a = Number(process.argv[2]);
-const b = Number(process.argv[3]);
-
 function add (a, b) {
   return a + b;
 }
-console.log(add(a, b));
+
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
