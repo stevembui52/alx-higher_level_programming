66696b01a2ed10f8a32c1d6bf53d56322838657d
stevembui52 <stevembui52@gmail.com>
2023-12-11 09:00:43 -0800

    multi

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* Script that prints 3 lines, and one loop */
+
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
