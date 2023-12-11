df721220314a88ea609e55e14e381c8815e96057
stevembui52 <stevembui52@gmail.com>
2023-12-11 08:53:23 -0800

    script that prints two arguments passed to it

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-/* Script that prints 3 lines: */
-
-const first = 'C is fun';
-const second = 'Python is cool';
-const third = 'Javascript is amazing';
-
-console.log(first);
-console.log(second);
-console.log(third);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
