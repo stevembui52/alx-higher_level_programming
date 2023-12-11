e09366aef706feb5281e6775a0e925f192c2db2e
stevembui52 <stevembui52@gmail.com>
2023-12-11 08:44:20 -0800

    script that prints the first argument passed to it

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* Script that prints the first argument passed to it */
+
+const arg = process.argv[2];
+if (arg) {
+  console.log(arg);
+} else {
+  console.log('No argument');
+}
