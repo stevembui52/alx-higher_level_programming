978e9927894dab2de76697c06d21792333b1e764
stevembui52 <stevembui52@gmail.com>
2023-12-12 10:34:04 -0800

    closures

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const square = require('./5-square');
+
+class Square extends square {
+  charPrint (c) {
+    if (typeof (c) === 'undefined') {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// function that returns the reversed version of a list.
+
+exports.esrever = function (str) {
+  const arr = [];
+  for (let i = str.lenght - 1; i => 0; i--) {
+    arr.push(str[i]);
+  }
+  return (arr);
+};
