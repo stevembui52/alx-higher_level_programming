c330f0af8e9ff787271328acc2472c46babbb281
stevembui52 <stevembui52@gmail.com>
2023-12-16 02:09:51 -0800

    task 8 -14 with sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""prints the State object with the name passed as argument from the database
+hbtn_0e_6_usa"""
+
+
+if __name__ == "__main__":
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+
+    found = False
+    for state in session.query(State):
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+            found = True
+            break
+    if found is False:
+        print("Not found")
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Adds the State object “Louisiana” to the
+database hbtn_0e_6_usa
+"""
+
+
+if __name__ == "__main__":
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+
+    add_state = State(name='Louisiana')
+    session.add(add_state)
+    session.commit()
+    print("{:d}".format(add_state.id))
+
+    session.close()
