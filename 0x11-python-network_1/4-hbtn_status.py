822bf6d6edc10257b35ba218a4bdb30cb4e7e666
stevembui52 <stevembui52@gmail.com>
2024-01-06 01:49:44 -0800

    with arguments

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,7 +7,7 @@ import requests
 
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
     r = requests.get(url)
     print('Body response:')
     print('\t- type: {}'.format(type(r.text)))
